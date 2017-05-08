13667710e80dbaddcb0b90204df51f4f48d31f77
mandliya <ravi.mandliya@gmail.com>
2017-05-07 23:52:19 -0700

    Adding the hello_world.rs

---
+++ hello_world.rs
@@ -0,0 +1,9 @@
+// This is a first customary hello world program in rust.
+//
+
+// the main function like c/c++
+fn main() {
+    
+    // println! is a macro.
+    println!("Hello World! I am the new Rustacian!");
+}
