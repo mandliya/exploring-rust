d0330fd3d5dc5966329452cc57d0fc757934a8ce
Ravi Mandliya <ramand@microsoft.com>
2017-05-13 00:15:51 -0700

    Updated example for fmt::Display

--- fmt_display.rs
+++ fmt_display.rs
@@ -24,9 +24,58 @@ impl fmt::Display for Record {
     }
 }
 
+#[derive(Debug)]
+struct Point(i32, i32);
+
+// fmt::Display implementation for Point
+//
+impl fmt::Display for Point {
+    fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "(x, y) ==> ({}, {})", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f32,
+    imag: f32,
+}
+
+// fmt::Display for complex
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
+#[derive(Debug)]
+struct List(Vec<i32>);
+
+// fmt::Display implementation which displays list in [index:value] format.
+//
+impl fmt::Display for List {
+    fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+        write!(f, "[")?;
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{} : {}", count, v)?;
+        }
+        write!(f, "]")
+    }
+}
 
 fn main() {
     let record = Record {name: "John Doe".to_string(), age: 32};
-    println!("Display: {}", record);
-    println!("Debug: {:?}", record);
+    let point = Point(0, 4);
+    let complex = Complex {real : 3.3, imag: 7.2};
+    let list = List(vec![2, 4, 6]);
+    println!("fmt::Display for Record : {}", record);
+    println!("fmt::Debug for Record: {:?}", record);
+    println!("fmt::Display for Point: {}", point);
+    println!("fmt::Debug for Point: {:?}", point);
+    println!("fmt::Display for Complex: {}", complex);
+    println!("fmt::Debug for Debug: {:?}", complex);
+    println!("fmt::Display for List: {}", list);
+    println!("fmt::Debug for List: {:?}", list);
 }
