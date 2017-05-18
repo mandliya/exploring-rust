6e82bd04dc03eee888680f8fe57577e7b9e1df6d
Ravi Mandliya <ramand@microsoft.com>
2017-05-18 00:08:22 -0700

    updated tuple examples

--- tuples.rs
+++ tuples.rs
@@ -6,8 +6,19 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
     (boolean, integer)
 }
 
+
+
 fn main() {
     let pair = (42, true);
     println!("Pair is {:?}", pair);
     println!("Reveresed pair is {:?}", reverse(pair));
+
+    let triplet = (3u8, 4u8, 5u8);
+    println!("triplets's first value: {}", triplet.0);
+    println!("triplets's second value: {}", triplet.1);
+
+    let tuple_of_tuples = ((1u32, -4i32), (-5.3f32, -11.4f32));
+    println!("Tuple of tuples: {:?}", tuple_of_tuples);
+    println!("Tuple of tuples[0]: {:?}", tuple_of_tuples.0);
+    println!("Tuple of tuples[0][1]: {:?}", (tuple_of_tuples.0).1);
 }
