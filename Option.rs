6e53b28ed3df981abfc940c062736d7e3364a714
Ravi Mandliya <ravi.mandliya@gmail.com>
2017-07-07 23:28:58 -0700

    Update Option.rs

--- Option.rs
+++ Option.rs
@@ -1,3 +1,5 @@
+/* This program demonstrates Rust's special enum Option<T> to catch Null dereferencing errors */
+
 fn checked_division(dividend : i32, divisor : i32) -> Option<i32> {
     if divisor == 0 {
         None
