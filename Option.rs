dd7b5aaeff52afce06ebc78fe0bd4ca6d8370f88
Ravi Mandliya <ravi.mandliya@gmail.com>
2017-07-07 23:27:38 -0700

    Create Option.rs

---
+++ Option.rs
@@ -0,0 +1,22 @@
+fn checked_division(dividend : i32, divisor : i32) -> Option<i32> {
+    if divisor == 0 {
+        None
+    } else {
+        Some(dividend / divisor)
+    }
+}
+
+fn try_division(dividend: i32, divisor: i32) {
+    match checked_division(dividend, divisor) {
+        None => println!("{} / {} divisoin failed!", dividend, divisor),
+        Some(quotient) => println!("{} / {} = {}", dividend, divisor, quotient),
+    }
+}
+
+fn main() {
+    let x = 9;
+    let y = 3;
+    try_division(x, y);
+    let y = 0;
+    try_division(x, y);
+}
